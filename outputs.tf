db2a471ec548165d033845105d5a81d5718e7736
zmunk <>
2025-06-04 16:59:00 +0300

    expose websocket connection handler iam policy arn

--- outputs.tf
+++ outputs.tf
@@ -42,3 +42,8 @@ output "connections_table_arn" {
   description = "ARN of connections dynamodb table"
   value       = aws_dynamodb_table.connections.arn
 }
+
+output "websocket_connection_handler_policy_arn" {
+  description = "ARN of IAM policy that allows handling API connections"
+  value       = aws_iam_policy.websocket_connection_handler_policy.arn
+}
