ad22915514f7cf9045f0b8ed113e3a73ffadf369
zmunk <>
2025-06-04 15:17:04 +0300

    add iam roles to outputs

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,18 @@ output "sendmessage_lambda_log_group" {
   description = "Log group name of websocket sendmessage function"
   value       = module.lambda_websocket_sendmessage.log_group_name
 }
+
+output "connect_lambda_iam_role_name" {
+  description = "IAM role name of websocket connect function"
+  value       = module.lambda_websocket_connect.role_name
+}
+
+output "disconnect_lambda_iam_role_name" {
+  description = "IAM role name of websocket disconnect function"
+  value       = module.lambda_websocket_disconnect.role_name
+}
+
+output "sendmessage_lambda_iam_role_name" {
+  description = "IAM role name of websocket sendmessage function"
+  value       = module.lambda_websocket_sendmessage.role_name
+}
