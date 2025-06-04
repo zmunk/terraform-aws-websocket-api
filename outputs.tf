b9a762a2c2ffcd68d37fd687b30285f345ed8498
zmunk <>
2025-06-04 16:31:45 +0300

    expose connections dynamodb table info

--- outputs.tf
+++ outputs.tf
@@ -32,3 +32,13 @@ output "sendmessage_lambda_iam_role_name" {
   description = "IAM role name of websocket sendmessage function"
   value       = module.lambda_websocket_sendmessage.role_name
 }
+
+output "connections_table_name" {
+  description = "Name of connections dynamodb table"
+  value       = aws_dynamodb_table.connections.name
+}
+
+output "connections_table_arn" {
+  description = "ARN of connections dynamodb table"
+  value       = aws_dynamodb_table.connections.arn
+}
